02134f2b1d4584fc8f0e0fd225c09be3f2c31871
ogbeche77 <ogbeche77@yahoo.com>
2020-09-03 15:22:40 +0200

    initial

---
+++ database.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+require('dotenv').config()
+
+var con = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query("CREATE DATABASE mydb", function (err, result) {
+        if (err) throw err;
+        console.log("Database created");
+    });
+});
+
+
